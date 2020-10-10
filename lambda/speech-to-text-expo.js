6c4e5cf256a8b668a440db5df6eab603a5b410ce
tkd708 <take708gym@gmail.com>
2020-10-11 03:31:47 +1000

    lambda

--- lambda/speech-to-text-expo.js
+++ lambda/speech-to-text-expo.js
@@ -108,16 +108,16 @@ const ffmpeg = __webpack_require__(/*! fluent-ffmpeg */ "fluent-ffmpeg");
 module.exports.handler = async function (event, context) {
   //console.log("queryStringParameters", event.queryStringParameters)
   const keys = {
-    type: process.env['type'],
-    project_id: process.env['project_id'],
-    private_key_id: process.env['private_key_id'],
-    private_key: process.env['private_key'],
-    client_email: process.env['client_email'],
-    client_id: process.env['client_id'],
-    auth_uri: process.env['auth_uri'],
-    token_uri: process.env['token_uri'],
-    auth_provider_x509_cert_url: process.env['auth_provider_x509_cert_url'],
-    client_x509_cert_url: process.env['client_x509_cert_url']
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url
   }; //console.log('test' + keys)
   //const client = new speech.SpeechClient({credentials: keys});
   //console.log(client)
