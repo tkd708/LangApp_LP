6176b1adb3de7101d511f39d2351721f3b7e19f7
tkd708 <take708gym@gmail.com>
2020-10-12 01:14:54 +1000

    lambda

--- lambda/hello.js
+++ lambda/hello.js
@@ -96,8 +96,8 @@
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 module.exports.handler = async function (event, context) {
   console.log("queryStringParameters", event.queryStringParameters); //const body = JSON.parse(event.body);
-  //console.log(body)
 
+  console.log(event.body);
   return {
     // return null to show no errors
     statusCode: 200,
@@ -111,11 +111,7 @@ module.exports.handler = async function (event, context) {
       txt: "sample text"
     })
   };
-}; // Now you are ready to access this API from anywhere in your Gatsby app! For example, in any event handler or lifecycle method, insert:
-// fetch("/.netlify/functions/hello")
-//    .then(response => response.json())
-//    .then(console.log)
-// For more info see: https://www.gatsbyjs.org/blog/2018-12-17-turning-the-static-dynamic/#static-dynamic-is-a-spectrum
+};
 
 /***/ })
 
