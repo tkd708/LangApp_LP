dfadd372997dcd804939b9f8b69fd85d69343ac7
tkd708 <take708gym@gmail.com>
2020-10-21 23:49:05 +1000

    twilio

--- lambda/api/twilio.js
+++ lambda/api/twilio.js
@@ -3,28 +3,30 @@ const twilio = require('twilio');
 const AccessToken = twilio.jwt.AccessToken;
 const { VideoGrant } = AccessToken;
 
-const generateToken = () => {
-  return new AccessToken(
-    process.env.GATSBY_TWILIO_ACCOUNT_SID,
-    process.env.GATSBY_TWILIO_API_KEY,
-    process.env.GATSBY_TWILIO_API_SECRET
-  );
-};
-
-const videoToken = (identity, room) => {
-  let videoGrant;
-  if (typeof room !== 'undefined') {
-    videoGrant = new VideoGrant({ room });
-  } else {
-    videoGrant = new VideoGrant();
-  }
-  const token = generateToken();
-  token.addGrant(videoGrant);
-  token.identity = identity;
-  return token;
-};
 
 module.exports.handler = async function(event, context) {
+
+    const generateToken = () => {
+        return new AccessToken(
+            process.env.GATSBY_TWILIO_ACCOUNT_SID,
+            process.env.GATSBY_TWILIO_API_KEY,
+            process.env.GATSBY_TWILIO_API_SECRET
+        );
+        };
+
+    const videoToken = (identity, room) => {
+        let videoGrant;
+        if (typeof room !== 'undefined') {
+            videoGrant = new VideoGrant({ room });
+        } else {
+            videoGrant = new VideoGrant();
+        }
+        const token = generateToken();
+        token.addGrant(videoGrant);
+        token.identity = identity;
+        return token;
+        };
+
     console.log('request: ' + event.body);
     console.log('request: ' + JSON.stringify(event.body));
 
