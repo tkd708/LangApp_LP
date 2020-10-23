34a0994da5a826ba8d82849c3e578311e05ed320
tkd708 <take708gym@gmail.com>
2020-10-24 02:09:30 +1000

    audio recorder inline if 2

--- src/components/AudioRecorder/AudioRecorder.js
+++ src/components/AudioRecorder/AudioRecorder.js
@@ -62,7 +62,7 @@ const AudioRecorder = () => {
 
     return (
       <div>
-          {(typeof window !== `undefined`) && //empty return works but ReactMic cause the same issue as importing
+          {window && //empty return works but ReactMic cause the same issue as importing
            <ReactMic
                 record={isRecording}
                 className="sound-wave"
