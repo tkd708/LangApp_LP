bc7a709fc534570bb482ee2fa3b2c08cac236c10
tkd708 <take708gym@gmail.com>
2020-10-24 00:57:06 +1000

    audio

--- src/components/AudioRecorder/AudioRecorder.js
+++ src/components/AudioRecorder/AudioRecorder.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { ReactMic } from 'react-mic';
+if (typeof window !== `undefined`) {
+    import { ReactMic } from 'react-mic';
+}
 import axios from 'axios';
 
 const AudioRecorder = () => {
