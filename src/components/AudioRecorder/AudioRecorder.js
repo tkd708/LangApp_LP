d623bd04df0e4d26aa0dccfb2274132a15d13014
tkd708 <take708gym@gmail.com>
2020-10-25 19:58:47 +1000

    transcribe longer

--- src/components/AudioRecorder/AudioRecorder.js
+++ src/components/AudioRecorder/AudioRecorder.js
@@ -100,7 +100,7 @@ const AudioRecorder = () => {
   const repeatRecoridng = () => {
     startRecording();
     console.log('repeated recording resumed')  
-    setTimeout(() => {stopRecording()}, 5000);
+    setTimeout(() => {stopRecording()}, 15000);
     console.log('repeated recording cut') 
   }
   const startLongRecording = () => {
