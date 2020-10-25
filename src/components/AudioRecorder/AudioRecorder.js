1a0156141efff774908695c1ce4b2b22403af261
tkd708 <take708gym@gmail.com>
2020-10-25 15:19:49 +1000

    transcribe longer

--- src/components/AudioRecorder/AudioRecorder.js
+++ src/components/AudioRecorder/AudioRecorder.js
@@ -10,6 +10,7 @@ import TranscribeLangs from './transcribeLangs.json';
 
 const AudioRecorder = () => {
     const [isRecording, setIsRecording] = useState(false);
+    const [isLongRecording, setIsLongRecording] = useState(false);
     const [blobRecorded, setBlobRecorded] = useState(null);
     const [recordString, setRecordString] = useState(null);
     const [transcriptChunk, setTranscriptChunk] = useState('');
@@ -31,7 +32,8 @@ const AudioRecorder = () => {
 
   const onStop = (recordedBlob) => {
     console.log('recordedBlob is: ', recordedBlob);
-    setBlobRecorded(recordedBlob)
+    setBlobRecorded(recordedBlob);
+    (isLongRecording === true) && repeatRecoridng();
   }
   
   const playRecording = () => {
@@ -74,8 +76,8 @@ const AudioRecorder = () => {
   const appendTranscript = () => {
       const appendedTranscript = [transcript, transcriptChunk]
       console.log(appendedTranscript)
-        setTranscript(appendedTranscript.join(' '));
-        //setTranscriptChunk('')
+      setTranscript(appendedTranscript.join(' '));
+      //setTranscriptChunk('')
   }
 
     useEffect(() => {
@@ -93,17 +95,23 @@ const AudioRecorder = () => {
         appendTranscript();
     }, [transcriptChunk])
 
-  const transcribeLongRecoridng = async () => {
+  const repeatRecoridng = async () => {
     startRecording()
-    console.log('started rec')  
+    console.log('repeated recording started')  
     await setTimeout(stopRecording, 10000);
-    console.log('ended rec')  
-      await blobToBase64()
-    console.log('converted blob')  
-      await sendGoogle()
-    console.log('sent audio')  
-      await appendTranscript()
+    console.log('repeated recording ended') 
   }
+
+  const startLongRecording = () => {
+    setIsLongRecording(true);
+    console.log('long recoding started')
+  }
+  const stopLongRecording = () => {
+    setIsLongRecording(false);
+    console.log('long recoding ended')
+  }
+
+
 
     return (
       <div>
@@ -132,14 +140,8 @@ const AudioRecorder = () => {
                 backgroundColor="transparent" />
                 }
         <button onClick={isRecording ? stopRecording : startRecording} type="button">{isRecording ? 'Stop Recording' : 'Start Recording'}</button>
-        <button onClick={playRecording} type="button">Play</button>
-        <button onClick={blobToBase64} type="button">Convert</button>
-        <button onClick={sendGoogle} type="button">Transcribe</button>
-        <button onClick={appendTranscript} type="button">Append Transcript</button>
-        <button onClick={transcribeLongRecoridng} type="button">Transcribe longer recording</button>
-        <p>TranscriptChunk below</p>
-        <p>{transcriptChunk}</p>
-        <p>TranscriptAppended below</p>
+        <button onClick={isLongRecording ? stopLongRecording : startLongRecording} type="button">{isLongRecording ? 'Stop long recording' : 'Start long recording'}</button>
+        <p>Transcript below</p>
             <p>{transcript}</p>
       </div>
 
