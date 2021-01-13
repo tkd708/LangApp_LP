5642a9cc4a86f96db0bcbdb150c8111a9b7c7d2a
tkd708 <take708gym@gmail.com>
2021-01-14 02:05:28 +1000

    chart js s3 test

--- lambda/api/chart.js
+++ lambda/api/chart.js
@@ -32,64 +32,73 @@ module.exports.handler = async function ( event, context, callback ) {
         }
     }
 
-    try {
-        const renderService = new CanvasRenderService( 800, 600 );
-        const options = {
-            type: 'bar',
-            data: {
-                labels: [ "Red", "Blue", "Yellow", "Green", "Purple", "Orange" ],
-                datasets: [ {
-                    label: '# of Votes',
-                    data: [ 12, 19, 3, 5, 2, 3 ],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
+    const renderService = new CanvasRenderService( 800, 600 );
+    console.log( 'graph render service...', renderService );
+
+    const options = {
+        type: 'bar',
+        data: {
+            labels: [ "Red", "Blue", "Yellow", "Green", "Purple", "Orange" ],
+            datasets: [ {
+                label: '# of Votes',
+                data: [ 12, 19, 3, 5, 2, 3 ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            } ]
+        },
+        options: {
+            scales: {
+                yAxes: [ {
+                    ticks: {
+                        beginAtZero: true
+                    }
                 } ]
-            },
-            options: {
-                scales: {
-                    yAxes: [ {
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    } ]
-                }
             }
-        };
-        const buffer = await renderService.renderToBuffer( options )
+        }
+    };
+    const buffer = await renderService.renderToBuffer( options )
+    console.log( 'rendered graph...', buffer );
 
-        const params = {
-            Bucket: 'langapp-audio-analysis',
-            Key: 'hoge.png',
-            Body: buffer,
-            ContentType: 'image/png',
-            ACL: 'public-read',
-        };
+    const params = {
+        Bucket: 'langapp-audio-analysis',
+        Key: 'hoge.png',
+        Body: buffer,
+        ContentType: 'image/png',
+        ACL: 'public-read',
+    };
 
-        const data = await s3.upload( params ).promise();
-        callback( null, {
-            statusCode: 200,
-            body: JSON.stringify( { url: data.Location } ),
-        } );
-    } catch( e ) {
-        console.log( e );
-        callback( null, {
-            statusCode: 500,
-            body: JSON.stringify( { message: 'unknown error!' } ),
-        } );
-    }
+    const data = await s3.upload( params )
+        .promise()
+        .then( ( data ) => {
+            console.log( "Full graph successfully uploaded to S3", data )
+            return ( data )
+        } )
+        .catch(
+            ( err ) => {
+                console.error( "Full graph upload to S3 error", err );
+            } );
+
+
+    //////////// Finish the api
+    let lambdaResponse = {
+        statusCode: 200,
+        headers: { "X-Line-Status": "OK" },
+        body: JSON.stringify( { url: data.Location } ),
+    };
+    context.succeed( lambdaResponse );
 }
