8c9e54ec512e6bb2241cc2e839e47b7f13a44f5c
tkd708 <take708gym@gmail.com>
2021-01-18 07:01:17 +1000

    media recorder polyfill

--- src/pages/audioRecorderLIFF.js
+++ src/pages/audioRecorderLIFF.js
@@ -13,12 +13,12 @@ import TranscribeLangs from '../constants/transcribeLangs.json';
 
 import { v4 as uuidv4 } from 'uuid';
 
-import AudioRecorder from "audio-recorder-polyfill"
+const AudioRecorder = typeof window !== `undefined` ? require( "audio-recorder-polyfill" ).default : '' //"window" is not available during server side rendering.
+//import AudioRecorder from "audio-recorder-polyfill"
 if( typeof window !== `undefined` ) {
     const ua = window.navigator.userAgent.toLowerCase();
     if( ua.indexOf( "iphone" ) !== -1 || ua.indexOf( "ipad" ) !== -1 ) { window.MediaRecorder = AudioRecorder }
 }
-//window.MediaRecorder = AudioRecorder
 
 const COMMON_WORDS = [
     'yes', 'no', 'yeah', 'ok', 'okay',
