6ea46e1829711e3a63549009e28867493d98d91d
tkd708 <take708gym@gmail.com>
2021-01-20 01:47:19 +1000

    LIFF sdk

--- src/pages/audioRecorderLIFF.js
+++ src/pages/audioRecorderLIFF.js
@@ -14,7 +14,8 @@ import TranscribeLangs from '../constants/transcribeLangs.json';
 import { v4 as uuidv4 } from 'uuid';
 
 //import liff from '@line/liff';
-const liff = typeof window !== `undefined` ? require( "@line/liff" ) : '' //"window" is not available during server side rendering.
+const liff = typeof window !== `undefined` ? require( "@line/liff" ) : '';//"window" is not available during server side rendering.
+( typeof window !== `undefined` ) && liff.init( { liffId: process.env.GATSBY_LINE_LIFFID } ).then( () => { } )
 
 //const AudioRecorder = typeof window !== `undefined` ? require( "audio-recorder-polyfill" ).default : '' //"window" is not available during server side rendering.
 //import AudioRecorder from "audio-recorder-polyfill"
@@ -100,7 +101,6 @@ const AudioRecorderLIFF = () => {
 
     const [ intervalSeconds, setIntervalSeconds ] = useState( 15 );
 
-    ( typeof window !== `undefined` ) && liff.init( { liffId: process.env.GATSBY_LINE_LIFFID } ).then( () => { } )
     useEffect( () => {
         liff.getProfile().then( profile => {
             setAppID( profile.displayName )
